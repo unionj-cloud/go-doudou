21ea0c42e2f568e9ef4e0507838eda9c102979d9
wubin1989 <328454505@qq.com>
2021-02-24 15:18:38 +0800

    ...

---
+++ kit/ip/ip.go
@@ -0,0 +1,18 @@
+package ip
+
+import (
+	"log"
+	"net"
+)
+
+func GetOutboundIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP
+}
