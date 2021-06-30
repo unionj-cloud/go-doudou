be0ae7c123f502733624dd9e086e99870b2a03cf
wubin1989 <328454505@qq.com>
2021-06-30 15:42:54 +0800

    v0.3.0

--- svc/internal/codegen/init.go
+++ svc/internal/codegen/init.go
@@ -87,7 +87,7 @@ require (
 	github.com/gorilla/handlers v1.5.1
 	github.com/sirupsen/logrus v1.8.1
 	github.com/go-resty/resty/v2 v2.6.0
-	github.com/unionj-cloud/go-doudou v0.2.9
+	github.com/unionj-cloud/go-doudou v0.3.0
 	github.com/olekukonko/tablewriter v0.0.5
 	github.com/ascarter/requestid v0.0.0-20170313220838-5b76ab3d4aee
 	github.com/common-nighthawk/go-figure v0.0.0-20200609044655-c4b36f998cf2
