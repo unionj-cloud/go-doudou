6217f70cb82fed64397f98603a4485f70431ad52
wubin1989 <328454505@qq.com>
2021-08-09 11:39:11 +0800

    support common parameters per endpoint & pie chart of api number stats in home page & add doc url in home page

--- svc/http/onlinedoc/handlerimpl.go
+++ svc/http/onlinedoc/handlerimpl.go
@@ -3,6 +3,8 @@ package onlinedoc
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
+	"github.com/unionj-cloud/go-doudou/svc/config"
 	"net/http"
 	"text/template"
 )
@@ -21,19 +23,28 @@ func (receiver *OnlineDocHandlerImpl) GetDoc(_writer http.ResponseWriter, _req *
 		tpl *template.Template
 		err error
 		buf bytes.Buffer
+		scheme string
+		host string
 	)
 	if tpl, err = template.New("handlerimpl.go.tmpl").Parse(indexTmpl); err != nil {
 		panic(err)
 	}
 	doc, _ := json.Marshal(Oas)
+	if _req.TLS == nil {
+		scheme = "http"
+	} else {
+		scheme = "https"
+	}
+	host = fmt.Sprintf("%s://%s%s", scheme, _req.Host, config.GddRouteRootPath.Load())
 	if err = tpl.Execute(&buf, struct {
 		Doc string
+		DocUrl string
 	}{
 		Doc: string(doc),
+		DocUrl: host + "/go-doudou/openapi.json",
 	}); err != nil {
 		panic(err)
 	}
-	_writer.WriteHeader(http.StatusOK)
 	_writer.Header().Set("Content-Type", "text/html; charset=utf-8")
 	_writer.Write(buf.Bytes())
 }
