848f29d9f1fe42bee57bb24833af33148c3768e4
wubin1989 <328454505@qq.com>
2021-09-09 17:20:58 +0800

    ...

--- ip/ip.go
+++ ip/ip.go
@@ -1,18 +1,12 @@
 package ip
 
 import (
-	"log"
 	"net"
 )
 
 func GetOutboundIP() net.IP {
-	conn, err := net.Dial("udp", "8.8.8.8:80")
-	if err != nil {
-		log.Fatal(err)
-	}
+	conn, _ := net.Dial("udp", "8.8.8.8:80")
 	defer conn.Close()
-
 	localAddr := conn.LocalAddr().(*net.UDPAddr)
-
 	return localAddr.IP
 }
