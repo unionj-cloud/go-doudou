8c1c3a43724e68f28ef096f779c62c841e5a5ff3
wubin1989 <328454505@qq.com>
2021-11-27 08:26:57 +0800

    ...

--- ddl/testdata/domain4/user.go
+++ ddl/testdata/domain4/user.go
@@ -6,12 +6,12 @@ import "time"
 type User struct {
 	ID         int    `dd:"pk;auto"`
 	Name       string `dd:"index:name_phone_idx,2;default:'jack'"`
-	Phone      string `dd:"index:name_phone_idx,1;default:'13552053960';extra:comment '手机号'"`
+	Phone      string `dd:"default:'13552053960';extra:comment '手机号'"`
 	Age        int    `dd:"unsigned"`
 	No         int    `dd:"type:int;unique"`
 	UniqueCol  int    `dd:"type:int;unique:unique_col_idx,1"`
 	UniqueCol2 int    `dd:"type:int;unique:unique_col_idx,2"`
-	School     string `dd:"null;default:'harvard';extra:comment '学校'"`
+	School     string `dd:"null;index:name_phone_idx,1;default:'harvard';extra:comment '学校'"`
 	IsStudent  bool
 	Rule       string `dd:"type:varchar(255);unique;extra:comment '链接匹配规则，匹配的链接采用该css规则来爬'"`
 	RuleType   string `dd:"type:varchar(45);extra:comment '链接匹配规则类型，支持prefix前缀匹配和regex正则匹配'"`
