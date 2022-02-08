b7c7af9489684c44ff3cbf6f9f76221a64ada5df
wubin1989 <328454505@qq.com>
2022-02-08 15:41:06 +0800

    ...

--- svc/http/model/model.go
+++ svc/http/model/model.go
@@ -1,7 +1,6 @@
 package model
 
 import (
-	"fmt"
 	"net/http"
 )
 
@@ -11,52 +10,4 @@ type Route struct {
 	Method      string
 	Pattern     string
 	HandlerFunc http.HandlerFunc
-}
-
-// BizError is used for business error implemented error interface
-// StatusCode will be set to http response status code
-// ErrCode is used for business error code
-// ErrMsg is custom error message
-type BizError struct {
-	StatusCode int
-	ErrCode    int
-	ErrMsg     string
-}
-
-type BizErrorOption func(bizError *BizError)
-
-func WithStatusCode(statusCode int) BizErrorOption {
-	return func(bizError *BizError) {
-		bizError.StatusCode = statusCode
-	}
-}
-
-func WithErrCode(errCode int) BizErrorOption {
-	return func(bizError *BizError) {
-		bizError.ErrCode = errCode
-	}
-}
-
-// NewBizError is factory function for creating an instance of BizError struct
-func NewBizError(err error, opts ...BizErrorOption) *BizError {
-	bz := &BizError{
-		ErrMsg: err.Error(),
-	}
-	for _, fn := range opts {
-		fn(bz)
-	}
-	return bz
-}
-
-// String function is used for printing string representation of a BizError instance
-func (b BizError) String() string {
-	if b.ErrCode > 0 {
-		return fmt.Sprintf("%d %s", b.ErrCode, b.ErrMsg)
-	}
-	return b.ErrMsg
-}
-
-// Error is used for implementing error interface
-func (b BizError) Error() string {
-	return b.String()
 }
