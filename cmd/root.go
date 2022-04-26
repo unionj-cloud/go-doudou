df3e122a17cf519e0eb9b1860e3892402af85649
wubin1989 <328454505@qq.com>
2022-04-26 12:56:59 +0800

    ...

--- cmd/root.go
+++ cmd/root.go
@@ -5,7 +5,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const version = "v1.0.6"
+const version = "v1.0.7"
 
 // rootCmd is the base command when called without any subcommands
 var rootCmd = &cobra.Command{
