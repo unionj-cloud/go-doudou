592d11fa33ba8698e7d9ca35fd70ffa811d0a5da
wubin1989 <328454505@qq.com>
2022-07-17 13:20:57 +0800

    fix unittest

--- toolkit/lab/typeregistry.go
+++ toolkit/lab/typeregistry.go
@@ -1,18 +1,18 @@
 package main
-
-import (
-	"fmt"
-	"github.com/unionj-cloud/go-doudou/toolkit/lab/vo"
-	"reflect"
-)
-
-var TypeRegistry = make(map[string]reflect.Type)
-
-func init() {
-	myTypes := []interface{}{vo.PageQuery{}, UsersvcImpl{}}
-	for _, v := range myTypes {
-		TypeRegistry[fmt.Sprintf("%T", v)] = reflect.TypeOf(v)
-	}
-	a := TypeRegistry
-	a = a
-}
+//
+//import (
+//	"fmt"
+//	"github.com/unionj-cloud/go-doudou/toolkit/lab/vo"
+//	"reflect"
+//)
+//
+//var TypeRegistry = make(map[string]reflect.Type)
+//
+//func init() {
+//	myTypes := []interface{}{vo.PageQuery{}, UsersvcImpl{}}
+//	for _, v := range myTypes {
+//		TypeRegistry[fmt.Sprintf("%T", v)] = reflect.TypeOf(v)
+//	}
+//	a := TypeRegistry
+//	a = a
+//}
