287339190654c2f6dc06cb93a6a38246df1221ae
wubin1989 <328454505@qq.com>
2022-07-20 16:40:11 +0800

    fix LargestRemainder bug when sum = 0

--- toolkit/numberutils/numberutils.go
+++ toolkit/numberutils/numberutils.go
@@ -34,24 +34,26 @@ func LargestRemainder(percentages []Percentage, places int32) {
 	for _, item := range decimalPercentages {
 		sum += item.Percentage.Value
 	}
-	for i, item := range decimalPercentages {
-		raw := decimal.NewFromFloat(float64(item.Percentage.Value*100) / float64(sum))
-		decimalPercentages[i].Percent = raw.RoundFloor(places)
-		decimalPercentages[i].Remainder = raw.Sub(decimal.NewFromFloat(percentages[i].Percent))
-	}
-	var curSum decimal.Decimal
-	for _, item := range decimalPercentages {
-		curSum = curSum.Add(item.Percent)
-	}
-	offset := decimal.New(1, -places)
-	limit := decimal.NewFromInt(100)
-	for curSum.LessThan(limit) {
-		sort.Slice(decimalPercentages, func(i, j int) bool {
-			return decimalPercentages[j].Remainder.LessThan(decimalPercentages[i].Remainder)
-		})
-		decimalPercentages[0].Percent = decimalPercentages[0].Percent.Add(offset)
-		decimalPercentages[0].Remainder = decimal.Decimal{}
-		curSum = curSum.Add(offset)
+	if sum > 0 {
+		for i, item := range decimalPercentages {
+			raw := decimal.NewFromFloat(float64(item.Percentage.Value*100) / float64(sum))
+			decimalPercentages[i].Percent = raw.RoundFloor(places)
+			decimalPercentages[i].Remainder = raw.Sub(decimal.NewFromFloat(percentages[i].Percent))
+		}
+		var curSum decimal.Decimal
+		for _, item := range decimalPercentages {
+			curSum = curSum.Add(item.Percent)
+		}
+		offset := decimal.New(1, -places)
+		limit := decimal.NewFromInt(100)
+		for curSum.LessThan(limit) {
+			sort.Slice(decimalPercentages, func(i, j int) bool {
+				return decimalPercentages[j].Remainder.LessThan(decimalPercentages[i].Remainder)
+			})
+			decimalPercentages[0].Percent = decimalPercentages[0].Percent.Add(offset)
+			decimalPercentages[0].Remainder = decimal.Decimal{}
+			curSum = curSum.Add(offset)
+		}
 	}
 	for _, item := range decimalPercentages {
 		item.Percentage.Percent, _ = item.Percent.Float64()
--- toolkit/numberutils/numberutils_test.go
+++ toolkit/numberutils/numberutils_test.go
@@ -26,3 +26,24 @@ func TestLargestRemainder(t *testing.T) {
 		fmt.Printf("%v\t%v\t%v\t%s\n", item.Data, item.Value, item.Percent, item.PercentFormatted)
 	}
 }
+
+func TestLargestRemainderZeroSum(t *testing.T) {
+	input := []numberutils.Percentage{
+		{
+			Value: 1,
+			Data:  "a",
+		},
+		{
+			Value: 0,
+			Data:  "b",
+		},
+		{
+			Value: 0,
+			Data:  "c",
+		},
+	}
+	numberutils.LargestRemainder(input, 3)
+	for _, item := range input {
+		fmt.Printf("%v\t%v\t%v\t%s\n", item.Data, item.Value, item.Percent, item.PercentFormatted)
+	}
+}
