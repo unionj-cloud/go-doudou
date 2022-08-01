d63bedeb08ead1ed5f06c94181e023ed1d17eedc
wubin1989 <328454505@qq.com>
2022-08-01 13:37:11 +0800

    ...

--- cmd/internal/svc/codegen/httpintegrationtesting_test.go
+++ cmd/internal/svc/codegen/httpintegrationtesting_test.go
@@ -2,11 +2,13 @@ package codegen
 
 import (
 	"fmt"
+	"github.com/unionj-cloud/go-doudou/toolkit/fileutils"
 	"path/filepath"
 	"testing"
 )
 
 func Test_notGenerated(t *testing.T) {
+	_ = fileutils.CreateDirectory(filepath.Join(testDir, "integrationtest"))
 	result := notGenerated(filepath.Join(testDir, "integrationtest"), filepath.Join(testDir, "testcode.postman_collection.json"))
 	fmt.Println(result)
 }
