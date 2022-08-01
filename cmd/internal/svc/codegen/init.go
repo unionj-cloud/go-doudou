0326cd25113e53d84eff0089929595098541d23e
wubin1989 <328454505@qq.com>
2022-08-01 13:38:53 +0800

    ...

--- cmd/internal/svc/codegen/init.go
+++ cmd/internal/svc/codegen/init.go
@@ -109,7 +109,7 @@ require (
 	github.com/opentracing/opentracing-go v1.2.0
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.8.1
-	github.com/unionj-cloud/go-doudou v1.1.9
+	github.com/unionj-cloud/go-doudou ` + version.Release + `
 )`
 
 const gitignoreTmpl = `# Binaries for programs and plugins
