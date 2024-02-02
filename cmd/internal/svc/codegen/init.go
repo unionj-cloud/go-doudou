0d430da4d52609c2afe99dd5901ae53b767f09a6
wubin1989 <328454505@qq.com>
2024-02-03 01:06:28 +0800

    add envconfig package and add values_by tag for support yaml list syntax

--- cmd/internal/svc/codegen/init.go
+++ cmd/internal/svc/codegen/init.go
@@ -116,7 +116,6 @@ require (
 	github.com/gorilla/handlers v1.5.1
 	github.com/iancoleman/strcase v0.1.3
 	github.com/jmoiron/sqlx v1.3.1
-	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/opentracing-contrib/go-stdlib v1.0.0
 	github.com/opentracing/opentracing-go v1.2.0
 	github.com/pkg/errors v0.9.1
