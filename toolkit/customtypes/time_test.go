ba8c64cf0d6f04c93dcd53aa6880fb2255b177c9
wubin48435 <wubin48435@hundsun.com>
2024-07-20 12:34:06 +0800

    fix customtypes.Time bug

---
+++ toolkit/customtypes/time_test.go
@@ -0,0 +1,34 @@
+package customtypes
+
+import (
+	"fmt"
+	"github.com/unionj-cloud/go-doudou/v2/toolkit/copier"
+	"testing"
+	"time"
+)
+
+type User struct {
+	CreatedAt Time `json:"created_at"`
+}
+
+type Author struct {
+	CreatedAt Time `json:"created_at"`
+}
+
+func TestTime_UnmarshalJSON(t *testing.T) {
+	u := User{}
+	fmt.Println(u.CreatedAt)
+	a := Author{}
+	copier.DeepCopy(&u, &a)
+	fmt.Println(a.CreatedAt)
+}
+
+func TestTime_UnmarshalJSON1(t *testing.T) {
+	u := User{
+		CreatedAt: Time(time.Now()),
+	}
+	fmt.Println(u.CreatedAt)
+	a := Author{}
+	copier.DeepCopy(&u, &a)
+	fmt.Println(a.CreatedAt)
+}
