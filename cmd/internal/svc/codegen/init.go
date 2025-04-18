7d59a92c984fdb97bc7156100acb21b33b2facb8
武斌 <wubinwill@gmail.com>
2025-04-19 00:05:37 +0800

    更新依赖项：将github.com/unionj-cloud/toolkit版本从v0.0.6升级至v0.0.7-0.20250418144858-a3b948354147，并将github.com/bytedance/sonic版本从v1.12.1升级至v1.13.2。

--- cmd/internal/svc/codegen/init.go
+++ cmd/internal/svc/codegen/init.go
@@ -131,7 +131,7 @@ require (
 	github.com/prometheus/client_golang v1.11.0
 	google.golang.org/grpc v1.38.0
 	google.golang.org/protobuf v1.26.0
-	github.com/bytedance/sonic v1.12.1
+	github.com/bytedance/sonic v1.13.2
 	github.com/unionj-cloud/toolkit v0.0.1
 	github.com/wubin1989/gen v0.0.5
 	github.com/unionj-cloud/go-doudou/v2 ` + version.Release + `
