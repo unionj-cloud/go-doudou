13a1e8b91a253948f7d5f42c11b5c045be4ff3dd
武斌 <wubin48435@hundsun.com>
2025-09-05 11:43:32 +0800

    fix: field.Tag nil

--- cmd/internal/svc/codegen/init.go
+++ cmd/internal/svc/codegen/init.go
@@ -132,7 +132,7 @@ require (
 	google.golang.org/grpc v1.38.0
 	google.golang.org/protobuf v1.26.0
 	github.com/bytedance/sonic v1.13.2
-	github.com/unionj-cloud/toolkit v0.0.1
+	github.com/unionj-cloud/toolkit v0.1.1
 	github.com/wubin1989/gen v0.0.4
 	github.com/unionj-cloud/go-doudou/v2 ` + version.Release + `
 )`
--- version/version.go
+++ version/version.go
@@ -1,3 +1,3 @@
 package version
 
-const Release = "v2.5.12"
+const Release = "v2.5.13"
