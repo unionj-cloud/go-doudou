a08c17fc23463a77fcb5382373e9272da5c281cd
wubin1989 <328454505@qq.com>
2026-04-25 03:27:50 +0800

    chore: remove unused go.uber.org/automaxprocs dependency and gomaxprocs metric

--- cmd/internal/templates/mainpkgmod.go
+++ cmd/internal/templates/mainpkgmod.go
@@ -104,7 +104,6 @@ require (
 	go.etcd.io/etcd/client/pkg/v3 v3.5.7 // indirect
 	go.etcd.io/etcd/client/v3 v3.5.7 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
-	go.uber.org/automaxprocs v1.5.2 // indirect
 	go.uber.org/goleak v1.1.12 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
 	go.uber.org/zap v1.23.0 // indirect
